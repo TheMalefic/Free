5f85c8331004d4ad44355c6190160bdb09e3a4ee
TheMalefic <56778381+TheMalefic@users.noreply.github.com>
2019-10-19 23:13:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!-- Proudly created by Joao -->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DIMAS-FF</title>
+	<meta name="description" content="Meu site é uma revolução dos games site: 'freefiredimas.tk'.">
+	<meta name="keywords" content=",eu site é uma revolução dos games site: 'freefiredimas.tk'.">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<script src="https://cdn.rawgit.com/leonardosnt/mc-player-counter/1.1.0/dist/mc-player-counter.min.js"></script>
+	<script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+	<script src="https://use.fontawesome.com/30155533f0.js"></script>
+	<script type="text/javascript" src="js/ss.js"></script>
+	<script type="text/javascript">
+		function mobile() {
+			var bars = document.getElementById("mnav");
+			if(bars.className === "mnav") {
+				bars.className += " responsive";
+			} else {
+				bars.className = "mnav";
+			}
+		}
+	</script>
+</head>
+<body>
+	<header>
+		<div class="nav">
+			<ul class="nav-items" id="nav-items">
+				<li onclick="mobile()" class="icon fa fa-bars fa-2x"></li>
+				<li class="h">
+					<div class="overlay"></div>
+					<a href="#main">Inicio</a>
+				</li>
+				<div class="log"><img alt="MyServer Logo" class="logo" src="img/logo1.png"></div>
+				<li class="h">
+					<div class="overlay"></div>
+					<a href="#staff">produtos</a>
+				</li>
+				<li class="h">
+					<div class="overlay"></div>
+					<a href="#contact">Contato</a>
+				</li>
+			</ul>
+			<div class="mnav" id="mnav">
+				<li class="i">
+					<a href="#main">Inicio</a>
+				</li>
+				<li class="i">
+					<a href="#staff">produtos</a>
+				</li>
+				<li class="i">
+					<a href="#contact">contato</a>
+				</li>
+			</div>
+		</div>
+
+		<div class="hero" id="main">
+			<h1>Bem vindo ao meu site</h1>
+			<p class="info">Divulgue: <span data-playercounter-ip="freefiredimas.tk">
+			65</span> Usuarios online <span>freefiredimas.tk</span></p>
+		</div>
+	</header>
+	<div class="main">
+		<div id="description">
+			<div>
+				<i class="fa fa-rocket fa-4x"></i>
+				<span>
+					<h3>nossa hístoria</h3>
+					<p>Estamos no ramo de venda de gift cards desde 2013! E cada dia crescenso mais com a ajuda de vocês</p>
+				</span>
+			</div>
+			<div>
+				<i class="fa fa-user fa-4x"></i>
+				<span>
+					<h3>pessoas que compraram:</h3>
+					<p>Atualmente todos compradores reagiram com 100% de ótimo atendimento e entregas rapidas!</p>
+				</span>
+			</div>
+			<div>
+				<i class="fa fa-server fa-4x"></i>
+				<span>
+					<h3>Nosso produtos</h3>
+					<p>Atualmente estamos no ramo de vendas de gift card, cartões pre pagos de diamantes free fire</p>
+				</span>
+			</div>
+		</div>
+		<div id="staff">
+			<div class="staff">
+				<h2>Produtos</h2>
+				<div class="staff-team">
+					<div>
+						<h3>Gift R$ 10,00 </h3>
+						<div class="owner">Preço: R$ 8,50</div>
+						<img alt="MyServer Staff Member" src="img/Gift.png">
+						<!-- INICIO DO BOTAO PAGSEGURO --><a href="pagamento.html" target="_blank" title="Pagar com PagSeguro"><img src="//assets.pagseguro.com.br/ps-integration-assets/botoes/pagamentos/205x30-pagar.gif" alt="Pague com PagSeguro - é rápido, grátis e seguro!" /></a><!-- FIM DO BOTAO PAGSEGURO -->
+					</div>
+
+					<div>
+						<h3>Gift R$ 15,00</h3>
+						<div class="owner">Preço: R$ 10,50</div>
+						<img alt="MyServer Staff Member" src="img/Gift.png">
+						<!-- INICIO DO BOTAO PAGSEGURO --><a href="pagamento.html" target="_blank" title="Pagar com PagSeguro"><img src="//assets.pagseguro.com.br/ps-integration-assets/botoes/pagamentos/205x30-pagar.gif" alt="Pague com PagSeguro - é rápido, grátis e seguro!" /></a><!-- FIM DO BOTAO PAGSEGURO -->
+					</div>
+
+					<div>
+						<h3>Gift R$ 25,00</h3>
+						<div class="manager">Preço: R$ 20,00</div>
+						<img alt="MyServer Staff Member" src="img/Gift.png">
+						<!-- INICIO DO BOTAO PAGSEGURO --><a href="pagamento.html" target="_blank" title="Pagar com PagSeguro"><img src="//assets.pagseguro.com.br/ps-integration-assets/botoes/pagamentos/205x30-pagar.gif" alt="Pague com PagSeguro - é rápido, grátis e seguro!" /></a><!-- FIM DO BOTAO PAGSEGURO -->
+					</div>
+
+					<div>
+						<h3>Gift R$ 35,00</h3>
+						<div class="admin">Preço: R$ 29,50</div>
+						<img alt="MyServer Staff Member" src="img/Gift.png">
+						<!-- INICIO DO BOTAO PAGSEGURO --><a href="pagamento.html" target="_blank" title="Pagar com PagSeguro"><img src="//assets.pagseguro.com.br/ps-integration-assets/botoes/pagamentos/205x30-pagar.gif" alt="Pague com PagSeguro - é rápido, grátis e seguro!" /></a><!-- FIM DO BOTAO PAGSEGURO -->
+					</div>
+
+					<div>
+						<h3>Gift R$ 50,00</h3>
+						<div class="admin">Preço: R$ 40,50</div>
+						<img alt="MyServer Staff Member" src="img/Gift.png">
+						<!-- INICIO DO BOTAO PAGSEGURO --><a href="pagamento.html" target="_blank" title="Pagar com PagSeguro"><img src="//assets.pagseguro.com.br/ps-integration-assets/botoes/pagamentos/205x30-pagar.gif" alt="Pague com PagSeguro - é rápido, grátis e seguro!" /></a><!-- FIM DO BOTAO PAGSEGURO -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="contact">
+			<h2>Contato</h2>
+			<p>Email - fogogratisfree0007@gmail.com</p>
+		</div>
+	</div>
+	<footer>
+		<div>
+			<img alt="MyServer Logo" src="img/logo1.png">
+			<p>Copyright &copy; DimasFF 2013-2019</p>
+		</div>
+		<div>
+			<p>Created By João</p>
+		</div>
+	</footer>
+</body>
+</html>
